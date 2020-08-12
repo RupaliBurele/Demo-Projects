d59131c2c28c3dca68ae4e1878c13f540eb15d63
RupaliBurele <69578395+RupaliBurele@users.noreply.github.com>
2020-08-12 15:25:39 +0530

    Update Demo.java

--- selenium GIT/src/com/demo/Demo.java
+++ selenium GIT/src/com/demo/Demo.java
@@ -6,6 +6,8 @@ public class Demo {
 		System.out.println("Hello Started using GIT hub");
 		
 		System.out.println("New changes was added successfully");
+		
+		System.out.println("pull code from Git Hub");
 	}
 
 }
