862450293732f175a25e66a584e526183618affe
Rupali <Rupali@192.168.1.11>
2020-08-12 15:23:29 +0530

    changes commit by Rb

--- selenium GIT/src/com/demo/Demo.java
+++ selenium GIT/src/com/demo/Demo.java
@@ -4,6 +4,8 @@ public class Demo {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello Started using GIT hub");
+		
+		System.out.println("New changes was added successfully");
 	}
 
 }
