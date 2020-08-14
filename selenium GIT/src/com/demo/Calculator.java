b116ed9de7398bcfb35f61e71cea0431bde593a9
Rupali <Rupali@192.168.1.11>
2020-08-14 16:01:15 +0530

    Commit new project by rups

---
+++ selenium GIT/src/com/demo/Calculator.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+public class Calculator {
+	public static void main(String[] args) {
+		int a=20, b=30, c;
+		c=a+b;
+		System.out.println("Summation Result="+c);
+
+		c=a-b;
+		System.out.println("Substraction Result="+c);
+
+		c=a*b;
+		System.out.println("Multiplication Result="+c);
+
+
+	}
+}
